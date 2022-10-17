5d72d9d84e39cc59f888ab22704bf29bb700f8b6
Arseny <Syp4ik22885@mail.ru>
2022-10-17 16:30:18 +0300

    Example_05

---
+++ Example_05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же Саша!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
